dc10e09842d704c1465ae1d559b6a632baf0b610
Lucas Peñalva <lucasarielpenalva@gmail.com>
2022-08-20 18:28:30 -0300

    Finished first Build

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+set -e
+
+npm run build
+
+cd dist
+
+
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:Lucas-Ariel-Penalva/<Unicorn-Trading>.git main:gh-pages
